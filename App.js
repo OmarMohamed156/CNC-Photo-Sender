3194f0a4db6da68f57cf608490d91b9fcf8c7814
OmarMohamed156 <om658036@gmail.com>
2022-05-16 23:45:19 +0200

    initial app is done

--- App.js
+++ App.js
@@ -1,11 +1,51 @@
+import axios from 'axios';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, View,Button,ToastAndroid,Image } from 'react-native';
+import { useState,useEffect } from 'react';
+import * as ImagePicker from 'expo-image-picker';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
 
 export default function App() {
+
+  const [image,setImage] = useState(null);
+  const [selected,setSelected] = useState(false);
+
+
+  const sendPhoto = async (result) => {
+    console.log(result.base64);
+    axios.post('http://',{
+      picture: `data:image/jpg;base64,${result.base64}`
+    }).then(res => {
+      console.log(res);
+      ToastAndroid.show('Photo sent',ToastAndroid.SHORT);
+    }).catch(err => {
+      console.log(err);
+      ToastAndroid.show('Error sending photo',ToastAndroid.SHORT);
+    }
+    )
+  }
+
+  const selectPhoto = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      base64: true,
+      allowsEditing: true,
+      quality: 1,
+    });
+    if (!result.cancelled) {
+      console.log(result);
+      setImage(result);
+      setSelected(true);
+    }
+    console.log(result);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {image && <Image source={image} style={styles.image} />}
+      {!selected && <Button title="Pick an image from camera roll" onPress={selectPhoto} ></Button>}
+      {selected && <Button title="Send photo" onPress={() => sendPhoto(image)} />}
     </View>
   );
 }
@@ -17,4 +57,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: 300,
+    height: 300,
+    marginVertical: 20,
+  },
+
 });
