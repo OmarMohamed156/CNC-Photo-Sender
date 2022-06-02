1c9eeff7c79868dd0ebe5d59b56fee9b5dfed4aa
OmarMohamed156 <om658036@gmail.com>
2022-06-02 03:30:22 +0200

    added shapes section

--- Shapes.js
+++ Shapes.js
@@ -1,10 +1,36 @@
-import { View ,Text,StyleSheet,SafeAreaView } from "react-native";
+import { View ,Text,StyleSheet,SafeAreaView,TouchableOpacity,ToastAndroid } from "react-native";
 import { Ionicons } from '@expo/vector-icons';
+import axios from "axios";
+import { useState } from "react";
 
 export default function Shapes(){
+
+    const sendShape= (shape)=>{
+        axios.post('http://',{
+            shape:shape
+        }).then(res=>{
+            console.log(res);
+            ToastAndroid.show('Shape sent',ToastAndroid.SHORT);
+        }).catch(err=>{
+            console.log(err);
+            ToastAndroid.show('Error Sending the Shape',ToastAndroid.SHORT);
+        })
+    }
+
+
     return(
         <View style={styles.container} >
-            <Text >Shapes</Text>
+            <Text style={styles.title} >Select the Shape ypu want to be drawn</Text>
+            <View style={styles.btnContainer} >
+                <TouchableOpacity style={styles.opacityBtn}>
+                    <Ionicons name="ios-square" size={80} color="#eb4034" onPress={()=>sendShape('square')} />
+                    <Text style={{marginTop:30,marginHorizontal:5,fontSize:15,}}>Square</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.opacityBtn}>
+                    <Ionicons name="ellipse-outline" size={80} color="#eb4034"  onPress={()=>sendShape('circle')}/>
+                    <Text style={{marginTop:30,marginHorizontal:5,fontSize:15,}}>Circle</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
@@ -13,7 +39,20 @@ const styles = StyleSheet.create({
     container:{
         flex:1,
         alignItems:'center',
-        justifyContent:'center',
         backgroundColor:'#fff',
-    }
+    },
+    title:{
+        fontSize:15,
+        marginTop:50,
+        fontWeight:'bold'
+    },
+    btnContainer:{
+        flex:1,
+        alignItems:'center',
+        width:'100%',
+        marginTop:20,
+    },
+    opacityBtn:{
+        flexDirection:'row',
+        marginVertical:10}
 })
